76fa5169b9d5c48693ecc31422364725b92fd6f3
Yuri Castro <yuri.castro.dev@gmail.com>
2026-06-10 07:45:08 -0300

    chore: initialize spring boot project with kafka, jpa and postgresql dependencies

---
+++ src/main/java/com/zs/kafka_log_pipeline/KafkaLogPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.zs.kafka_log_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLogPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaLogPipelineApplication.class, args);
+	}
+
+}
